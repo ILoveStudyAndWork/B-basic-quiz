7a981af01f28fa69a99ce40d76bdb8569a8da605
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-22 23:06:19 +0800

    [fengyi_ling] feat:add user repository test

---
+++ src/test/java/com/thoughtworks/gtb/bbasicquiz/repository/UserRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.thoughtworks.gtb.bbasicquiz.repository;
+
+import com.thoughtworks.gtb.bbasicquiz.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .name("Panda")
+                .age(24L)
+                .avatar("http://...")
+                .description("A good guy.")
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+        entityManager.clear();
+    }
+
+    @Test
+    void should_return_user_when_id_exists() {
+        entityManager.persistAndFlush(user);
+
+        Optional<User> found = userRepository.findById(1L);
+
+        assertThat(found.isPresent()).isTrue();
+        assertThat(found.get()).isEqualTo(User.builder()
+                .id(1L)
+                .name("Panda")
+                .age(24L)
+                .avatar("http://...")
+                .description("A good guy.")
+                .build());
+    }
+
+    @Test
+    void should_optional_when_id_not_exists() {
+        entityManager.persistAndFlush(user);
+        Optional<User> found = userRepository.findById(3L);
+
+        assertThat(found.isPresent()).isFalse();
+    }
+}
